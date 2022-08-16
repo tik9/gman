9ee493ba20820034940ba680324d647a460f6b89
tiko <tik_win10>
2022-08-16 14:08:08 +0200

    curl-api-github-description-newname-change-work

--- change_repo.sh
+++ change_repo.sh
@@ -1,28 +1,39 @@
 
 ghtoken=$(cat $HOME/gman/ghtoken)
+echo $ghtoken
 user=tik9
-repo_api=https://api.github.com/repos/$user
-repo=apps
+base=https://api.github.com
+repo_api=$base/repos/$user
+all_repos=$base/users/$user/repos
+repo=fun
+
+function all_repo { curl -s $all_repos ;}
 
 function newname {
-     new_repo=test
-     res=$(jq -n --arg name "$new_repo" '{name:$name}')
+     res=fun
+     res=$(jq -n --arg name $res '{name:$name}')
      echo $res
 }
 
-function description {
-     description='A jekyll static site for applications with help of javascript'
+function get_description {
+     res=$(curl -s $repo_api/$repo)
+     echo $(echo "$res"|jq '.description' )
+}
+
+function ch_description {
+     description='Serverless functions on node js'
      res=$(jq -n --arg name $repo --arg descr "$description" '{name:$name,description:$descr}')
      echo $res
 }
 
-function test {
-     curl -i -u $user:$ghtoken https://api.github.com/users/octocat
-     # curl $repo_api/$repo
-}
+function test { curl $repo_api/$repo ;}
 
-test
-res=$(description)
-# newname
-# echo $res
-# curl -u "$user:$ghtoken" --request PATCH --data "$res" $repo_api/$repo
+# test
+# all_repo
+# res=$(ch_description)
+get_description
+# res=$(newname)
+# curl -w '%{response_code}' 'https://api.github.com/users/tik9'
+
+# descrip: curl -H "Authorization: token OAUTH-TOKEN" --request PATCH --data '{"name":"repo", "description":"a new description"}' https://api.github.com/repos/:owner/:repo
+curl -s -u "$user:$ghtoken" --request PATCH --data "$res" $repo_api/$repo
